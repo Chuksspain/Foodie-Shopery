7b257bc1ee9ed462d943ca7212f5654ed4706818
Chuksspain <chuksspain@gmail.com>
2025-09-09 10:59:10 +0100

    commit

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const btnEL = document.querySelector(".menu-btn");
-const divEL = document.querySelector("div.nav-telephonefour");
-btnEL.addEventListener('click', function () {
-    divEL.classlist.toggle("nav-open")
-})
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+    const btn = document.querySelector(".menu-btn");
+    // const headerEL = document.querySelector("header.main-headie");
+    btnNavEL.addEventListener('click', function () {
+        toggle("nav-open")
+    })
+
+}
